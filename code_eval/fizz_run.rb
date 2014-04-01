1e4a82ef654695f45bb62d26def1c83e7b144380
Derik Strattan <derik.strattan@gmail.com>
2014-04-01 11:35:11 -0700

    add quick sort function

--- code_eval/fizz_run.rb
+++ code_eval/fizz_run.rb
@@ -1,13 +1,15 @@
 File.open('input.txt').each_line do |line|
   split = line.split(" ")
   ary = Array.new
+  A = split[0].to_i
+  B = split[1].to_i
+  N = split[2].to_i
 
-  (1..split[2].to_i).each do |i|
+  (1..N).each do |i|
     x = ''
-    x += 'F' if i % split[0].to_i == 0
-    x += 'B' if i % split[1].to_i == 0
+    x += 'F' if i % A == 0
+    x += 'B' if i % B == 0
     ary << (x.empty? ? i : x)
   end
   puts ary * (" ")
 end
-
---
+++ code_wars/quick_sort.rb
@@ -0,0 +1,18 @@
+# Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+# For example:
+
+# solution([1, 2, 10, 50, 5]) # should return [1,2,5,10,50]
+# solution(nil) # should return []
+
+def solution(nums)
+  if  nums == nil
+    Array.new
+  else
+    sort_nums = nums.clone # clone the nums array
+    return sort_nums if sort_nums.size <= 1
+    pivot = sort_nums.pop # pops the last number out of the cloned array
+    left, right = sort_nums.partition { |e| e < pivot } #cloned array is partitioned
+    solution(left) + [pivot] + solution(right)
+  end
+end
