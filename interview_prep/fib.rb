2d209c60564c01b46838fd918c5f3c5c29324ce7
Derik Strattan <derik.strattan@gmail.com>
2014-04-06 16:59:08 -0700

    add multiplication and square root

--- interview_prep/fib.rb
+++ interview_prep/fib.rb
@@ -1,5 +1,6 @@
 # Find the nth number of a fibonacci sequence
 
+# this solution is not good for large numbers because of recursion
 def fibonacci(num)
   return num if [0, 1].include? num
   fibonacci(num - 2) + fibonacci(num - 1)
@@ -9,7 +10,8 @@ p fibonacci(4)
 p fibonacci(5)
 p fibonacci(7)
 
-# is fibonacci number?
+
+# Given a number, is the number a fibonacci number?
 
 def is_fibonacci?(num)
   arr = [0, 1]
@@ -22,4 +24,13 @@ def is_fibonacci?(num)
   return false
 end
 
+# Find the nth number of a fibonacci sequence
+def fib(a, b = 1, c = 0, count = 2)
+  return 0 if a == 0
+  return 1 if a == 1
+  return b + c if count == a
+  fib(a, b + c, b, count += 1)
+end
 
+p fib(2)
+p fib(100)
